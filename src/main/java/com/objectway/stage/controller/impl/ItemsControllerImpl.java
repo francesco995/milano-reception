5774bcad83f4a62da3fad40e1ff9f2b19a0c9e51
Francesco Gianni <francesco.gianni@objectway.it>
2017-11-10 16:45:06 +0100

    fix dependency

--- src/main/java/com/objectway/stage/controller/impl/ItemsControllerImpl.java
+++ src/main/java/com/objectway/stage/controller/impl/ItemsControllerImpl.java
@@ -1,13 +1,14 @@
 package com.objectway.stage.controller.impl;
 
 import com.objectway.stage.controller.ItemsController;
+import com.objectway.stage.repo.ItemsRepository;
 import com.objectway.stage.repo.model.Item;
 
 import java.util.List;
 
 public class ItemsControllerImpl implements ItemsController {
 
-
+    private ItemsRepository itemsRepository;
 
     public List<Item> getAllItems() {
         return null;
