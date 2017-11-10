e7850a4e79e1597fa1b6e94f0295e6e85e3d4555
Marco <you@example.com>
2017-11-10 17:06:11 +0100

    Item and User implementation

--- src/main/java/com/objectway/stage/repo/model/Item.java
+++ src/main/java/com/objectway/stage/repo/model/Item.java
@@ -1,4 +1,34 @@
 package com.objectway.stage.repo.model;
 
-public class Item {
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    private Integer idRef;
+    private String name;
+    private String description;
+    private Float price;
+    private Integer amount;
+
+
+    public Integer getIdRef() { return idRef;}
+
+    public void setIdRef(Integer idRef) {this.idRef = idRef;}
+
+    public String getName() { return name;}
+
+    public void setName(String name) { this.name = name;}
+
+    public String getDescription() { return description;}
+
+    public void setDescription(String description) { this.description = description;}
+
+    public Float getPrice() { return price;}
+
+    public void setPrice(Float price) { this.price = price;}
+
+    public Integer getAmount() { return amount;}
+
+    public void setAmount(Integer amount) { this.amount = amount;}
+
 }
--- src/main/java/com/objectway/stage/repo/model/User.java
+++ src/main/java/com/objectway/stage/repo/model/User.java
@@ -1,4 +1,39 @@
 package com.objectway.stage.repo.model;
 
-public class User {
+import java.io.Serializable;
+import java.util.List;
+
+public class User implements Serializable {
+
+    private Integer idRef;
+    private String name;
+    private String cognome;
+    private String ruolo;
+    private List<String> userName;
+    private List<String> password;
+
+
+    public Integer getIdRef() { return idRef;}
+
+    public void setIdRef(Integer idRef) {this.idRef = idRef;}
+
+    public String getName() { return name;}
+
+    public void setName(String name) { this.name = name;}
+
+    public String getCognome() { return cognome;}
+
+    public void setCognome(String cognome) { this.cognome = cognome;}
+
+    public String getRuolo() { return ruolo;}
+
+    public void setRuolo(String ruolo) { this.ruolo = ruolo;}
+
+    public List<String> getUserName() { return userName;}
+
+    public void setUserName(List<String> userName) { this.userName = userName;}
+
+    public List<String> getPassword() { return password;}
+
+    public void setPassword(List<String> password) { this.password = password;}
 }
