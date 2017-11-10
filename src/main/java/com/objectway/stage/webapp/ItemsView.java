16c8d6095c9642455c2bc06ebd0045b06822fa5d
fabrizio <fabrizio.stella@objectway.it>
2017-11-10 17:56:48 +0100

    ItemsList

---
+++ src/main/java/com/objectway/stage/webapp/ItemsView.java
@@ -0,0 +1,41 @@
+package com.objectway.stage.webapp;
+
+import com.objectway.stage.repo.model.Item;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@Controller
+public class ItemsController implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+
+
+	public final static String SESSION_CUSTOMERS_PARAM = "items";
+
+
+
+	@RequestMapping("/items.html")
+	public String viewItems(Model model) {
+
+		return "itemsTiles";
+	}
+
+	@ModelAttribute(SESSION_CUSTOMERS_PARAM)
+	public List<Item> items() {
+		return this.applicationContext.getBean(ItemsController.class).items();
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+}
