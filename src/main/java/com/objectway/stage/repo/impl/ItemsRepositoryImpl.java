7fb199aa2b8993a086a0d6f3fd6d5555dc0727e3
vschiral <vito.schiraldi@objectway.it>
2017-11-14 11:02:26 +0100

    Turned class into singleton and implemented a dummy repository for test purposes

--- src/main/java/com/objectway/stage/repo/impl/ItemsRepositoryImpl.java
+++ src/main/java/com/objectway/stage/repo/impl/ItemsRepositoryImpl.java
@@ -9,7 +9,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class ItemsRepositoryImpl implements ItemsRepository {
+public final class ItemsRepositoryImpl implements ItemsRepository {
+
+    private static ItemsRepositoryImpl instance = null;
+    private ItemsRepositoryImpl() {
+        // should load file or whatever here
+        repository = new HashMap<String, Item>();
+        // what follows is just a stub and will be removed upon actual persistency implementation
+        Item lapis = new Item();
+            lapis.setIdRef("LPS-01");
+            lapis.setName("Matita HB");
+            lapis.setDescription("Matita HB con gommino, dimensione 13cm");
+            lapis.setAmount(10);
+            lapis.setPrice(0.25f);
+            repository.put(lapis.getIdRef(),lapis);
+        Item band = new Item();
+            band.setIdRef("BND-04");
+            band.setName("Elastico grande");
+            band.setDescription("Elastico ad alto spessore per faldoni, larghezza 0.8cm, lunghezza a riposo (2x)12cm, colore verde");
+            band.setAmount(50);
+            band.setPrice(0.05f);
+            repository.put(band.getIdRef(),band);
+    }
+
+    public static ItemsRepositoryImpl getInstance() {
+        if (instance == null) {
+            instance = new ItemsRepositoryImpl();
+        }
+        return instance;
+    }
+
 
     private HashMap<String, Item> repository;
 
